5f99f82b2d698dd43b05e6ee19c86716d9fd9484
sainath31 <sainath31@icloud.com>
2026-08-14 23:54:22 +0000

    Restore GitHub Pages base path and HashRouter until Cloudflare Pages is connected

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 
@@ -11,9 +11,9 @@ document.addEventListener('load', (e) => {
 const root = createRoot(document.getElementById('root'))
 root.render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
 
